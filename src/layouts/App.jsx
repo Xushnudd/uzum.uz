ae74a9aadf60ed8713c1d9076d5607e1086538d7
Ali Ibrahim <babayevagulnova005@gmail.com>
2026-03-28 09:24:15 +0400

    created site

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,8 +1,10 @@
 import Uzum from "../pages/Uzum"
+import Market from "../pages/Market"
 function App() {
     return (
         <>
 <Uzum/>
+<Market/>
         </>
     )
 }
---
+++ src/pages/Market.jsx
@@ -0,0 +1,12 @@
+function Market(){
+    return(<>
+    <div>
+        <div className="font-bold text-3xl">Arzon narxlar kafolati<i class="fa-solid fa-chevron-right mt-10" ></i> </div>
+        <img src="phone.png" alt="" className="h-75 w-75 rounded-2xl mt-15"/>
+        <div className="font-bold text-blue-600 text-2xl">2.789.000</div>
+        <div className=" text-gray-500 ">2.987.300</div>
+        <div className="bg-yellow-300 w-max p-1 pl-5 pr-5 rounded">127.400 so'm/oyiga</div>
+    </div>
+    </>)
+}
+export default Market
