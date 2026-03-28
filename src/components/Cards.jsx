bdd4f3e5f7d39e90abaffe45059165eaf1dd2489
Ulug'bek <uk2315652@gmail.com>
2026-03-28 09:30:19 +0500

    fast commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,60 @@
 function Cards(){
     return ( 
 <div className="flex justify-between">
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
+<Cards />
 
 </div>
     )
 }
+export default Cards
