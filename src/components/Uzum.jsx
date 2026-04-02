72a2070fdaf76a36d6ca2ae602475aa7565b2e25
Xushnudd <boshhacker1@gmail.com>
2026-04-02 22:03:26 +0500

    done

--- src/components/Uzum.jsx
+++ src/components/Uzum.jsx
@@ -1,48 +1,48 @@
 import { Link } from "react-router-dom"
 import Katalog from "./Katalog"
-
-function showCity() {
-  return (
-    <div className="absolute left-[30%] top-0 z-20">
-      <Katalog />
-    </div>
-  )
-}
+import { useState } from "react"
 
 function Uzum() {
-  return (<>
-    <div className="bg-gray-200 flex pt-3 pb-3">
-      <div className="pl-5 pr-5" onClick={""}><i className="fa-solid fa-location-dot" ></i>Toshkent<i class="fa-solid fa-chevron-down" ></i></div>
-      <Link to={"/uz/about/delivery-points"} className="text-gray-800 pr-75">Topshirish punktlari</Link>
-      <Link to={"https://seller.uzum.uz/"} className="text-blue-700 font-bold border border-l-gray-200 border-t-gray-200 border-b-gray-200 border-r-gray-500 pr-5">Sotuvchi bo'lish</Link>
-      <Link to={"https://promo.uzum.uz/uz/promo/pvz"} className="text-blue-700 font-bold pl-5 pr-5">Topshirish punktini ochish</Link>
-      <Link to={"/uz/faq"} className="text-gray-600 ">Savol-javob</Link>
-      <div className="text-gray-600 pr-5">Buyurtmalarim</div>
-      <img src="/uzbek flag.png" alt="" className="w-5 h-5 rounded-full" />   <div className="pr-5 pl-1">O'zbekcha</div>
-    </div>
+  const [st, setst] = useState(false)
 
-    <div className="flex mt-5">
-      <img src="/uzum2.png" alt="" className="w-10 h-10 rounded-full ml-5" />  <div className="text-blue-600 text-2xl font-bold pl-5">UZUM MARKET </div>
-      <div className="text-blue-600 border border-blue-100 w-max bg-blue-100  rounded p-1 pl-5 pr-5 ml-10"><i className="fa-solid fa-diagram-next pr-5" ></i>Katalog</div>
-      <input type="text" placeholder="Mahsulotlar Izlash" className="border border-gray-300 pr-50 pl-10 ml-10" />
-      <i className="fa-solid fa-magnifying-glass  bg-gray-200 pr-10 pt-3 pl-5 text-gray-500" ></i>
-      <div className="pl-10"><i className="fa-solid fa-user pr-5" ></i>Kirish</div>
-      <div className="pl-10"><i className="fa-solid fa-heart pr-5"></i>Saralangan</div>
-      <div className="pl-10"><i className="fa-solid fa-cart-shopping pr-5" ></i>Savat</div>
-    </div>
+  function changeSt() {
+    setst(!st)
+  }
 
-    <div className="mt-5 flex">
-      <div className="pl-5"><i className="fa-solid fa-fire pr-5" ></i>Hafta tovarlari</div>
-      <div className="pl-5"><i className="fa-solid fa-shirt pr-5"></i>Bahorgi kolleksiya</div>
-      <div className="pl-5"><i className="fa-solid fa-pump-soap pr-5" ></i>Sizning go'zalligingiz</div>
-      <div className="pl-5"><i className="fa-solid fa-guitar pr-5"></i>Xobbi va ijod</div>
-      <div className="pl-5"><i className="fa-solid fa-mobile-screen pr-5" ></i>Smartfonlari</div>
-      <div className="pl-5"><i className="fa-solid fa-rug pr-5" ></i>Ramazon Hayiti</div>
-      <div className="pl-5 text-gray-500">Mebel</div>
-      <div className="pl-5 text-gray-500">Turizm,baliq ovi va ovchilik</div>
-      <div className="pl-5 text-gray-500">Yana<i className="fa-solid fa-chevron-down" ></i></div>
-    </div>
-    {showCity()}
-  </>)
+  return (
+    <>
+      <div className="bg-gray-200 flex pt-3 pb-3">
+        <div onClick={changeSt} className="px-5 cursor-pointer"><i className="fa-solid fa-location-dot" ></i>Toshkent<i class="fa-solid fa-chevron-down" ></i></div>
+        <Link to={"/uz/about/delivery-points"} className="text-gray-800 pr-75">Topshirish punktlari</Link>
+        <Link to={"https://seller.uzum.uz/"} className="text-blue-700 font-bold border border-l-gray-200 border-t-gray-200 border-b-gray-200 border-r-gray-500 pr-5">Sotuvchi bo'lish</Link>
+        <Link to={"https://promo.uzum.uz/uz/promo/pvz"} className="text-blue-700 font-bold pl-5 pr-5">Topshirish punktini ochish</Link>
+        <Link to={"/uz/faq"} className="text-gray-600 ">Savol-javob</Link>
+        <div className="text-gray-600 pr-5">Buyurtmalarim</div>
+        <img src="/uzbek flag.png" alt="" className="w-5 h-5 rounded-full" />   <div className="pr-5 pl-1">O'zbekcha</div>
+      </div>
+
+      <div className="flex mt-5">
+        <img src="/uzum2.png" alt="" className="w-10 h-10 rounded-full ml-5" />  <div className="text-blue-600 text-2xl font-bold pl-5">UZUM MARKET </div>
+        <div className="text-blue-600 border border-blue-100 w-max bg-blue-100  rounded p-1 pl-5 pr-5 ml-10"><i className="fa-solid fa-diagram-next pr-5" ></i>Katalog</div>
+        <input type="text" placeholder="Mahsulotlar Izlash" className="border border-gray-300 pr-50 pl-10 ml-10" />
+        <i className="fa-solid fa-magnifying-glass  bg-gray-200 pr-10 pt-3 pl-5 text-gray-500" ></i>
+        <div className="pl-10"><i className="fa-solid fa-user pr-5" ></i>Kirish</div>
+        <div className="pl-10"><i className="fa-solid fa-heart pr-5"></i>Saralangan</div>
+        <div className="pl-10"><i className="fa-solid fa-cart-shopping pr-5" ></i>Savat</div>
+      </div>
+
+      <div className="mt-5 flex">
+        <div className="pl-5"><i className="fa-solid fa-fire pr-5" ></i>Hafta tovarlari</div>
+        <div className="pl-5"><i className="fa-solid fa-shirt pr-5"></i>Bahorgi kolleksiya</div>
+        <div className="pl-5"><i className="fa-solid fa-pump-soap pr-5" ></i>Sizning go'zalligingiz</div>
+        <div className="pl-5"><i className="fa-solid fa-guitar pr-5"></i>Xobbi va ijod</div>
+        <div className="pl-5"><i className="fa-solid fa-mobile-screen pr-5" ></i>Smartfonlari</div>
+        <div className="pl-5"><i className="fa-solid fa-rug pr-5" ></i>Ramazon Hayiti</div>
+        <div className="pl-5 text-gray-500">Mebel</div>
+        <div className="pl-5 text-gray-500">Turizm,baliq ovi va ovchilik</div>
+        <div className="pl-5 text-gray-500">Yana<i className="fa-solid fa-chevron-down" ></i></div>
+      </div>
+      <div onClick={changeSt}><Katalog on={st}/></div>
+    </>)
 }
 export default Uzum
