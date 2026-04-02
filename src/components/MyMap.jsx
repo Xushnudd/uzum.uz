deadc935fa24bbc2111b74264e947c3dc28bebb1
Xushnudd <boshhacker1@gmail.com>
2026-04-02 09:16:32 +0500

    done

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,8 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-// Muhim: Marker ikonkalari ba'zan React-da yo'qolib qoladi, 
-// uni to'g'rilash uchun quyidagi importlarni qo'shish kerak bo'lishi mumkin
 import L from 'leaflet';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
@@ -19,13 +17,12 @@ function MyMap({ width = "100%", height = "100%", latitude, longitude }) {
     const position = [latitude, longitude];
 
     return (
-        // Dinamik o'lchamlar uchun style ishlatamiz
         <div style={{ width: width, height: height }}>
             <MapContainer 
                 center={position} 
                 zoom={13} 
                 scrollWheelZoom={false} 
-                style={{ height: "100%", width: "100%" }} // Xarita konteynerni to'ldirishi kerak
+                style={{ height: "100%", width: "100%" }}
             >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
