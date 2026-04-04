7feafe38cb2ccc8eba3e02ce3e9145047e63f207
Xushnudd <boshhacker1@gmail.com>
2026-04-04 22:53:03 +0500

    add Cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,23 @@
+import Card2 from "../../components/Card2";
+import Footer from "../../components/Footer";
 import Uzum from "../../components/Uzum";
 
 const Cart = () => (
     <div className="w-full h-screen">
         <Uzum />
-        <div className="w-full h-[40%]">
-            <div className="flex flex-col justify-center items-center">
+        <div className="w-full h-[40%] my-20">
+            <div className="w-full h-full flex flex-col justify-center items-center gap-10">
                 <img src="/basket/catUzum.png" alt="Mushuk rasmi" className="w-30" />
+                <div className="w-100 text-center">
+                    <h2 className="py-10 text-2xl font-bold">Savatingiz hozircha bo‘sh</h2>
+                    <p>Bosh sahifadan boshlang — kerakli tovarni qidiruv orqali topishingiz yoki to‘plamlarni ko‘rishingiz mumkin</p>
+                    <button className="my-5 bg-gray-200 font-bold p-2 rounded">Bosh sahifa</button>
+                </div>
             </div>
         </div>
+        <Card2 />
+        <div className="h-10"></div>
+        <Footer />
     </div>
 )
 
