75719a73ae9f7aac0cce30310b7d21bb02bcc4f8
Xushnudd <boshhacker1@gmail.com>
2026-04-04 22:58:21 +0500

    add Cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Card2 from "../../components/Card2";
 import Footer from "../../components/Footer";
 import Uzum from "../../components/Uzum";
@@ -11,7 +12,9 @@ const Cart = () => (
                 <div className="w-100 text-center">
                     <h2 className="py-10 text-2xl font-bold">Savatingiz hozircha bo‘sh</h2>
                     <p>Bosh sahifadan boshlang — kerakli tovarni qidiruv orqali topishingiz yoki to‘plamlarni ko‘rishingiz mumkin</p>
-                    <button className="my-5 bg-gray-200 font-bold p-2 rounded">Bosh sahifa</button>
+                    <Link to={"/"}>
+                    <button className="my-5 bg-gray-200 font-bold p-2 rounded cursor-pointer hover:bg-gray-300">Bosh sahifa</button>
+                    </Link>
                 </div>
             </div>
         </div>
