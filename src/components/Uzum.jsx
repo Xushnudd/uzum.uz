cf6927a9f05cf408738ef268b2abbd816a32e9d7
= <=>
2026-04-10 14:13:53 +0500

    done

--- src/components/Uzum.jsx
+++ src/components/Uzum.jsx
@@ -36,30 +36,32 @@ function Uzum() {
         </div>
       </div>
 
-      <div className="w-full flex mt-5">
-        <Link to={"/"} className="flex"><img src="/uzum2.png" alt="" className="w-10 h-10 rounded-full ml-5" />  <div className="text-blue-600 text-2xl font-bold pl-5">UZUM MARKET </div></Link>
-        <div className="text-blue-600 border border-blue-100 w-max bg-blue-100  rounded p-1 pl-5 pr-5 ml-10"><i className="fa-solid fa-diagram-next pr-5" ></i>Katalog</div>
-        <input type="text" placeholder="Mahsulotlar Izlash" className="border border-gray-300 pr-50 pl-10 ml-10" />
-        <i className="fa-solid fa-magnifying-glass  bg-gray-200 pr-10 pt-3 pl-5 text-gray-500" ></i>
-        <div onClick={changePh} className="pl-10 cursor-pointer"><i className="fa-solid fa-user pr-5" ></i>Kirish</div>
-        <div className="pl-10"><i className="fa-solid fa-heart pr-5"></i>Saralangan</div>
-        <Link to={"/uz/cart"} className="pl-10"><i className="fa-solid fa-cart-shopping pr-5" ></i>Savat</Link>
-      </div>
+      <div className="mx-auto w-300">
+        <div className="w-full flex mt-5">
+          <Link to={"/"} className="flex"><img src="/uzum2.png" alt="" className="w-10 h-10 rounded-full ml-5" />  <div className="text-blue-600 text-2xl font-bold pl-5">UZUM MARKET </div></Link>
+          <div className="text-blue-600 border border-blue-100 w-max bg-blue-100  rounded p-1 pl-5 pr-5 ml-10"><i className="fa-solid fa-diagram-next pr-5" ></i>Katalog</div>
+          <input type="text" placeholder="Mahsulotlar Izlash" className="border border-gray-300 pr-50 pl-10 ml-10" />
+          <i className="fa-solid fa-magnifying-glass  bg-gray-200 pr-10 pt-3 pl-5 text-gray-500" ></i>
+          <div onClick={changePh} className="pl-10 cursor-pointer"><i className="fa-solid fa-user pr-5" ></i>Kirish</div>
+          <div className="pl-10"><i className="fa-solid fa-heart pr-5"></i>Saralangan</div>
+          <Link to={"/uz/cart"} className="pl-10"><i className="fa-solid fa-cart-shopping pr-5" ></i>Savat</Link>
+        </div>
 
-      <div className="w-full mt-5 flex">
-        <div className="pl-5"><i className="fa-solid fa-fire pr-5" ></i>Hafta tovarlari</div>
-        <div className="pl-5"><i className="fa-solid fa-shirt pr-5"></i>Bahorgi kolleksiya</div>
-        <div className="pl-5"><i className="fa-solid fa-pump-soap pr-5" ></i>Sizning go'zalligingiz</div>
-        <div className="pl-5"><i className="fa-solid fa-guitar pr-5"></i>Xobbi va ijod</div>
-        <div className="pl-5"><i className="fa-solid fa-mobile-screen pr-5" ></i>Smartfonlari</div>
-        <div className="pl-5"><i className="fa-solid fa-rug pr-5" ></i>Ramazon Hayiti</div>
-        <div className="pl-5 text-gray-500">Mebel</div>
-        <div className="pl-5 text-gray-500">Turizm,baliq ovi va ovchilik</div>
-        <div className="pl-5 text-gray-500">Yana<i className="fa-solid fa-chevron-down" ></i></div>
-      </div>
-      <div onClick={changeSt}><Katalog on={st} /></div>
-      <div>
-        {ph ? <div className="absolute left-1/3 z-30"><PhoneInput /></div> : null}
+        <div className="w-full mt-5 flex">
+          <div className="pl-5"><i className="fa-solid fa-fire pr-5" ></i>Hafta tovarlari</div>
+          <div className="pl-5"><i className="fa-solid fa-shirt pr-5"></i>Bahorgi kolleksiya</div>
+          <div className="pl-5"><i className="fa-solid fa-pump-soap pr-5" ></i>Sizning go'zalligingiz</div>
+          <div className="pl-5"><i className="fa-solid fa-guitar pr-5"></i>Xobbi va ijod</div>
+          <div className="pl-5"><i className="fa-solid fa-mobile-screen pr-5" ></i>Smartfonlari</div>
+          <div className="pl-5"><i className="fa-solid fa-rug pr-5" ></i>Ramazon Hayiti</div>
+          <div className="pl-5 text-gray-500">Mebel</div>
+          <div className="pl-5 text-gray-500">Turizm,baliq ovi va ovchilik</div>
+          <div className="pl-5 text-gray-500">Yana<i className="fa-solid fa-chevron-down" ></i></div>
+        </div>
+        <div onClick={changeSt}><Katalog on={st} /></div>
+        <div>
+          {ph ? <div className="absolute left-1/3 z-30"><PhoneInput /></div> : null}
+        </div>
       </div>
     </>)
 }
